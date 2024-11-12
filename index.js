864d944cc294f71908d451063eb0a694dcfa09ab
ranamiah221 <ranarasul21@gmail.com>
2024-11-13 00:09:35 +0600

    created find one api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app= express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,19 @@ async function run() {
     await client.connect();
     
     const coffeesCollection=client.db('coffeeDB').collection('coffees')
+//   coffees collection 
+    app.get('/coffees', async(req, res)=>{
+        const cursor= coffeesCollection.find();
+        const result = await cursor.toArray();
+        res.send(result) 
+    })
+
+   app.get('/coffees/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query= {_id : new ObjectId(id)}
+        const result= await coffeesCollection.findOne(query)
+        res.send(result) 
+   })
 
     app.post('/coffees', async(req, res)=>{
         const coffee = req.body;
@@ -36,15 +49,7 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/coffees', async(req, res)=>{
-        const cursor= coffeesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result) 
-    })
-
-
-
-
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
